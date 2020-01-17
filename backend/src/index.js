26e361490f4d0501223ae31ddcd193d5fd2ea114
Luccas Traumer <lucas.jeesus@hotmail.com>
2020-01-17 12:37:09 -0300

    without key acess my database

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const routes = require('./routes');
 // Utilizadas
 const app = express();
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb+srv://traumer:luccas@cluster0-x0aop.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect('******',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
